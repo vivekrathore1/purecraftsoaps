a8794c3f7526616de6418e96c81671b8b33fcbd2
Vivek Rathore <133784178+vivekrathore1@users.noreply.github.com>
2026-07-03 23:17:19 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,6 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbysILpisfJopG6ht3PQ5v_mr2HwooM-aLWUaPz1iaxQ6T_pLsuYGG3WtWDoGcZsAFVoYA/exec";
+
+fetch(SCRIPT_URL,{
+  method:"POST",
+  body:JSON.stringify(formData)
+})
