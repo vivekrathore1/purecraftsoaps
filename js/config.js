faf03f85b3a4e0f2dc2dad314bdd9206f83286bb
Vivek Rathore <133784178+vivekrathore1@users.noreply.github.com>
2026-07-03 23:04:47 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+
+  SCRIPT_URL: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
+
+  COMPANY_NAME: "PureCraft Soaps",
+
+  EMAIL: "export@purecraftsoaps.in",
+
+  PHONE: "+91-7210884633",
+
+  WHATSAPP: "+91-9953570717"
+
+};
