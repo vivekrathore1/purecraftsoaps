70f1f44f51f809e5ccc6dc8d6e690f764fc2dd4b
Vivek Rathore <133784178+vivekrathore1@users.noreply.github.com>
2026-07-03 23:40:52 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,10 @@
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxxxpgdB_pgdK0vWqmvT9A97XSpOTSYSP_UUzma33HYNYZDOSVWuddUV_mfhW1nuTdpZg/exec";
+const CONFIG  = {
+
+  // Paste your Apps Script Web App URL here
+  SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxxxpgdB_pgdK0vWqmvT9A97XSpOTSYSP_UUzma33HYNYZDOSVWuddUV_mfhW1nuTdpZg/exec",
+
+  SUCCESS_MESSAGE:
+    "✓ Inquiry sent! We'll respond within 24 hours."
+
+};
 
