b4a406a9acc67ad58d7761dc0bff17c586ba7542
Vivek Rathore <133784178+vivekrathore1@users.noreply.github.com>
2026-07-03 21:19:21 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,12 +1,46 @@
-document.getElementById("contactForm")
-.addEventListener("submit", function(e){
+<script>
+document
+.getElementById("contactForm")
+.addEventListener("submit", async function(e){
 
-e.preventDefault();
+  e.preventDefault();
 
-alert(
-"Thank you! We will contact you soon."
-);
+  const formData = {
+    name: document.querySelector(
+      'input[placeholder="Your Full Name *"]'
+    ).value,
 
-this.reset();
+    email: document.querySelector(
+      'input[type="email"]'
+    ).value,
+
+    company: document.querySelector(
+      'input[placeholder="Company / Business Name"]'
+    ).value,
+
+    country: document.querySelector(
+      'input[placeholder="Country *"]'
+    ).value,
+
+    message: document.querySelector(
+      "textarea"
+    ).value
+  };
+
+  const response = await fetch(
+    "1RW4uXSd5gusi16qR0sNuQ0uPjviW9fBdd2vBNDTpUCA_VyxNxp4_B3I4",
+    {
+      method: "POST",
+      body: JSON.stringify(formData)
+    }
+  );
+
+  if(response.ok){
+      alert("Inquiry submitted!");
+      this.reset();
+  }else{
+      alert("Submission failed");
+  }
 
 });
+</script>
